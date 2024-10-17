11a11ef958069fb3f93f5b33d2343e47b38ffdff
karthika <karthikasbabugmail.com>
2024-10-17 16:17:17 +0530

    first commit

---
+++ responsive-navbar-router2/src/Components/Product.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useParams} from 'react-router-dom';
+
+
+const Product = () => {
+    const { id } = useParams();
+  
+
+    const productDetails = {
+        1: "Details about Product 1",
+        2: "Details about Product 2",
+        3: "Details about Product 3"
+    };
+
+    const productDetail = productDetails[id] || "Product not found";
+
+    return (
+        <div className="container">
+            <h1>Product {id}</h1>
+            <p>{productDetail}</p>
+           </div>
+    );
+};
+
+export default Product;
